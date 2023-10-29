7244be1981b79cce2c27e7bc455ba68f4ecff029
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-29 21:43:02 +0100

    feat: add confetti

--- src/components/Confetti.jsx
+++ src/components/Confetti.jsx
@@ -1,13 +1,10 @@
-const { useMyContext } = require("@/store/passportStore")
-import ReactConfetti from 'react-confetti';
-import { useWindowSize } from 'react-use';
+import { useMyContext } from "@/store/passportStore"
+import ReactConfetti from 'react-confetti'
+import {useWindowSize} from 'react-use'
 
-
-export default function CustomConfetti() {
-  const [width, height] = useWindowSize()
+export default function Confetti() {
+  const { width, height } = useWindowSize()
   const { showConfetti } = useMyContext()
-
-
   return (
     <>
       {showConfetti && <ReactConfetti width={width} height={height} />}

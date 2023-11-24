61c5231583ddaa3d2fcb7e965d89371e888dcf7f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-11-24 18:23:49 +0100

    feat: reset redis store

--- src/pages/api/token.js
+++ src/pages/api/token.js
@@ -1,13 +1,15 @@
 import { createRedisInstance } from "@/utils/redis";
 
+const KEY_PREFIX = "TOKEN_ID"
+
 export default async function handler(
   req,
   res
 ) {
   const redis = createRedisInstance();
-  const tokenId = await redis.get("tokenid");
+  const tokenId = await redis.get(KEY_PREFIX);
   let x = Number(tokenId) || 1;
   const y = x + 1
-  await redis.set("tokenid", y);
+  await redis.set(KEY_PREFIX, y);
   res.status(200).json({ tokenId: x });
 }
